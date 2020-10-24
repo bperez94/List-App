cf76497bef9a3dd72814dcadaae9f629414a809f
Brandon Perez <brandonp940@gmail.com>
2020-10-24 11:49:16 -0400

    able to seed data

---
+++ seed.js
@@ -0,0 +1,32 @@
+var seeder = require('mongoose-seed');
+// Connect to MongoDB via Mongoose
+seeder.connect('mongodb://localhost/list', function() {
+  // Load Mongoose models
+  seeder.loadModels([
+    'modules/listModel.js',
+    
+  ]);
+  // Clear specified collections
+  seeder.clearModels(['list'], function() {
+    // Callback to populate DB once collections have been cleared
+    seeder.populateModels(data, function() {
+      seeder.disconnect();
+    });
+  });
+});
+// Data array containing seed data - documents organized by Model
+var data = [
+	{
+		'model': 'list',
+		'documents': [
+			{
+				'name': 'Doc1',
+				'value': "city"
+			},
+			{
+				'username': 'Doc2',
+				'value': "DC"
+			}
+		]
+	}
+];
