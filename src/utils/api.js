ffda3b4e2dfec16661fdfc36cd38421b7f6ed9bf
Brandon Perez <brandonp940@gmail.com>
2020-10-26 21:49:59 -0400

    added for mongodb use

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,21 @@
-import axios from "axios";
+//import axios from "axios";
 
-export default {
-
-};
+// export default {
+//     getLists: function () {
+//         return axios.get("api/users/");
+        
+//     },
+    
+//     getList: function (id) {
+//         return axios.get("api/user/"+ id);
+//     },
+    
+//     deleteList: function(id) {
+//         return axios.delete("/api/user/"+ id);
+//     },
+    
+//     saveList: function(userData) {
+//         return axios.post("/api/user", userData);
+//     }
+    
+// };
